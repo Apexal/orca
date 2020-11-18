52b7a4b8540f19a68d7d55afd14c4a9a96cac061
Frank Matranga <thefrankmatranga@gmail.com>
2020-11-17 21:03:15 -0500

    Add models

--- server.py
+++ server.py
@@ -1,8 +1,36 @@
-from typing import Optional
+from typing import List, Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Course(BaseModel):
+    semester: str
+    title: str
+    subject_prefix: str
+    code: str
+    description: str
+    when_offered: str
+
+
+class CourseSection(BaseModel):
+    section: str
+    crn: str
+
+
+class CourseSectionPeriod(BaseModel):
+    type: str
+    credits: List[int]
+    start_time: Optional[str]
+    end_time: Optional[str]
+    days: List[int]
+    intruction_method: Optional[str] = None
+    instructor: str
+    max_enrollments: int
+    enrollments: int
+    textbooks_url: str
 
 
 @app.get("/")
