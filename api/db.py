c90139bd1875951b12255d67037c242124fdc593
Frank Matranga <thefrankmatranga@gmail.com>
2020-11-23 15:49:26 -0500

    Method to update database records from parsing

--- api/db.py
+++ api/db.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import Any, List, Dict
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from .models import CourseSection, CourseSectionPeriod
@@ -9,6 +9,40 @@ load_dotenv(find_dotenv())
 
 conn = psycopg2.connect(
     os.environ["POSTGRES_DSN"], cursor_factory=RealDictCursor)
+
+
+def update_course_sections(semester_id: str, course_sections: List[CourseSection]):
+    c = conn.cursor()
+
+    c.execute("DELETE FROM course_section_periods")
+    c.execute("DELETE FROM course_sections")
+    # c.execute(
+    #     'UPDATE course_sections SET removed=true WHERE semester_id=%s', (semester_id,))
+    c.execute('SELECT crn FROM course_sections WHERE semester_id=%s',
+              (semester_id,))
+
+    existing_crns = c.fetchall()
+    print(existing_crns)
+    for course_section in course_sections:
+        record = course_section.to_record()
+
+        # Add new record
+        placeholders = ','.join(
+            map(lambda key: f'%({key})s', record.keys()))
+        c.execute(
+            f'INSERT INTO course_sections({",".join(record.keys())}) VALUES ({placeholders})', record)
+        print(f'Added new course_section {record["crn"]}')
+
+        # Add course sections
+        for period in course_section.periods:
+            record = period.to_record()
+            placeholders = ','.join(
+                map(lambda key: f'%({key})s', record.keys()))
+            c.execute(
+                f'INSERT INTO course_section_periods({",".join(record.keys())}) VALUES ({placeholders})', record)
+            print(
+                f'Added {period.class_type} period for {course_section.crn}')
+    conn.commit()
 
 
 def fetch_course_section(semester_id: str, crn: str) -> CourseSection:
@@ -40,7 +74,7 @@ def fetch_course_section_periods(semester_id: str, crn: str) -> List[CourseSecti
     return list(map(_raw_to_course_section_period, course_section_periods_raw))
 
 
-def _raw_to_course_section_period(raw: Dict[str, any]) -> CourseSectionPeriod:
+def _raw_to_course_section_period(raw: Dict[str, Any]) -> CourseSectionPeriod:
     if raw['instructors']:
         raw['instructors'] = raw['instructors'].split('/')
     if raw['days']:
